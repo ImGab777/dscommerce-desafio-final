6dceb9d107663458a163922c476f4bbc75889626
Gabriel <gabeefesouzask8@gmail.com>
2026-03-04 15:01:15 -0300

    entidades de segurança (user e role)

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dscommerce.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {}
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authority);
+    }
+}
